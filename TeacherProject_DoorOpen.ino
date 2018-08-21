47da0e0c92c032f78c4bd0633ec5b38e62baf448
Chumbaka <32573947+Chumbaka@users.noreply.github.com>
2018-08-21 16:21:05 +0800

    Add files via upload

---
+++ TeacherProject_DoorOpen.ino
@@ -0,0 +1,39 @@
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+
+int sensorPin = A0;    // select the input pin for the potentiometer
+int sensorPin2 = A1;    // select the input pin for the potentiometer
+int ledPin = 13;      // select the pin for the LED
+int sensorValue = 0;  // variable to store the value coming from the sensor
+int sensorValue2 = 0;  // variable to store the value coming from the sensor
+
+void setup() {
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+  sensorValue2 = analogRead(sensorPin2);
+  Serial.print(sensorValue);
+  Serial.print(" ; ");
+  Serial.println(sensorValue2);
+
+  if (sensorValue <500 or sensorValue2 <500 ){
+    myservo.write(0);
+    delay(1000);
+      for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+
+  }
+}
